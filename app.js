2e9decfcafce1c178b19b1e2d95772db2506d603
mateusz <mateusz.basiak@gmail.com>
2018-11-24 16:22:27 +0100

    module export method

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-console.log('starting app.');
+// console.log('starting app.');
 
-const fs = require('fs'); //require module fs
-// const os = require('os'); //fequire moduls os , take a look at  nodejs.org/api
-const _ = require('lodash')
-const notes=require('./notes.js') //require a file 
+// const fs = require('fs'); //require module fs
+// // const os = require('os'); //fequire moduls os , take a look at  nodejs.org/api
+// const _ = require('lodash')
+// const notes=require('./notes.js') //require a file 
 
 // console.log(_.isString('mateusz '));//return true or false
 // console.log(_.uniq(['maaat',1,'maaat',1,2,3,4,4,5,4]));//uniq removes duplicates from array
@@ -27,3 +27,46 @@ const notes=require('./notes.js') //require a file
 //         console.log('unable to write to file')
 //     }
 // });
+
+console.log('starting app.');
+
+const fs = require('fs'); //require module fs
+const _ = require('lodash');
+const yargs = require('yargs')
+
+const notes = require('./notes.js'); //require a file 
+
+const argv = yargs.argv
+const command = process.argv[2];
+console.log('Command:', command);
+// console.log('process:',process.argv);
+console.log("yargs :",argv);
+
+
+// if (command === 'add') {
+//     console.log('adding new note');
+// } else if (command === "list") {
+//     console.log('listing all notes');
+// } else {
+//     console.log(`command '${command}' not recognized`)
+// }
+
+switch (command) {
+    case 'add':
+        // console.log('Adding new notes');
+        notes.addNote(argv.title, argv.body);
+        break;
+    case 'list':
+        // console.log('Listing all notes');
+        notes.getAll();
+        break;
+    case 'remove':
+       notes.removeNote(argv.title);
+        break;
+    case 'read':
+      notes.getNote(argv.title);
+        break;
+    default:
+        console.log(`command '${command}' not recognized`);
+
+}
--- notes.js
+++ notes.js
@@ -4,14 +4,37 @@ console.log('starting notes.js');
 
 // module.exports.age = 25;
 
-module.exports.addNote =()=>{
-    console.log('add note');
-    return 'New Note'
+// module.exports.addNote =()=>{
+//     console.log('add note');
+//     return 'New Note'
 
-}
+// }
 
 // module.exports.add =(a,b)=>{
 //     console.log(`a equals ${a} and b equals ${b}  sum equals ${a+b}`);
 //     return a+b;
 
 // }
+
+const addNote=(title,body)=>{
+console.log('adding note', title,body);
+};
+
+const getAll=()=>{
+    console.log('getting all notes');
+}
+
+const removeNote=(title)=>{
+    console.log(`romoving note ${title}` );
+}
+const getNote=(title)=>{
+    console.log(`geting note ${title}`);
+}
+
+module.exports={
+   addNote, // es6 syntax for  addNote:addNote  
+   getAll,
+   removeNote,
+  getNote
+
+}
