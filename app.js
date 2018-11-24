b1f72db00cf9ebeb4b6637b821b812e078730860
mateusz <mateusz.basiak@gmail.com>
2018-11-24 02:24:39 +0100

    using module export

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ console.log('starting app.');
 
 const fs = require('fs'); //require module fs
 const os = require('os'); //fequire moduls os , take a look at  nodejs.org/api
+const notes=require('./notes.js') //require a file 
+
+let res = notes.addNote();
+console.log(res);
+
+let add = notes.add(5,10);
+console.log(add)
 
 
-let user = os.userInfo();
+// let user = os.userInfo();
 
-// console.log(user);
-fs.appendFile('greetings.txt', `Hello ${user.username} !`, function (err) {
-    if (err) {
-        console.log('unable to write to file')
-    }
-});
+// // console.log(user);
+// fs.appendFile('greetings.txt', `Hello ${user.username}  you are ${notes.age} !`, function (err) {
+//     if (err) {
+//         console.log('unable to write to file')
+//     }
+// });
---
+++ notes.js
@@ -0,0 +1,17 @@
+console.log('starting notes.js');
+
+// console.log(module);
+
+// module.exports.age = 25;
+
+module.exports.addNote =()=>{
+    console.log('add note');
+    return 'New Note'
+
+}
+
+module.exports.add =(a,b)=>{
+    console.log(`a equals ${a} and b equals ${b}  sum equals ${a+b}`);
+    return a+b;
+
+}
